897fe0159b4c33350af48b7439c4b4129f20fdcd
ouhell <oussamabarca200@gmail.com>
2023-09-19 14:41:38 +0100

    added success and error message in contact

---
+++ components/Message/index.tsx
@@ -0,0 +1,65 @@
+import { AnimatePresence } from "framer-motion";
+import React from "react";
+import { motion as m } from "framer-motion";
+import classNames from "classnames";
+export type MessageProps = {
+  message?: string;
+  id?: string;
+  type: "error" | "success";
+};
+
+const Message = ({ message, id, type }: MessageProps) => {
+  const resetter = React.useRef<ReturnType<typeof setTimeout>>();
+  const [show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    if (message) {
+      clearTimeout(resetter.current);
+
+      resetter.current = setTimeout(() => {
+        setShow(false);
+      }, 3000);
+
+      setShow(true);
+    }
+  }, [id]);
+
+  return (
+    <AnimatePresence>
+      {show && (
+        <m.div
+          initial={{
+            opacity: 0,
+            y: -50,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            transition: {
+              duration: 0.5,
+              ease: "easeInOut",
+            },
+          }}
+          exit={{
+            opacity: 0,
+            scale: 0.6,
+          }}
+          className="fixed top-0 w-full  text-white p-4"
+        >
+          <div
+            className={classNames(
+              "w-fit bg-gray-400 rounded-md  mx-auto  p-2 text-xs",
+              {
+                "bg-red-500": type === "error",
+              }
+            )}
+          >
+            {message}
+          </div>
+        </m.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Message;
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -2,6 +2,7 @@ import Input from "@/components/Input";
 import React from "react";
 import emailjs from "@emailjs/browser";
 import { BiLoader } from "react-icons/bi";
+import Message, { MessageProps } from "@/components/Message";
 type Props = {};
 
 type FormData = {
@@ -63,6 +64,11 @@ const initialFormData: Record<string, FormData> = {
 const Contact = (props: Props) => {
   const form = React.useRef<HTMLFormElement>(null);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [notifMessage, setNotifMessage] = React.useState<MessageProps>({
+    message: "",
+    id: "",
+    type: "success",
+  });
   const [formData, setFormData] = React.useState(initialFormData);
 
   const validate = (): boolean => {
@@ -101,8 +107,19 @@ const Contact = (props: Props) => {
         .then(
           (result) => {
             console.log(result.text);
+            setNotifMessage({
+              id: Math.random() + "",
+              message: "Message sent",
+              type: "success",
+            });
+            setFormData(initialFormData);
           },
           (error) => {
+            setNotifMessage({
+              id: Math.random() + "",
+              message: "Unexpected Error",
+              type: "error",
+            });
             console.log(error.text);
           }
         )
@@ -124,78 +141,85 @@ const Contact = (props: Props) => {
   };
 
   return (
-    <form
-      ref={form}
-      action="/api/hello"
-      method="POST"
-      className="h-full container flex flex-col justify-center items-center  mx-auto gap-8"
-      target="lmao"
-      onSubmit={(e) => {
-        e.preventDefault();
-        sendEmail();
-      }}
-    >
-      <h1 className="text-4xl">
-        Lets <span className="text-accent">Connect</span>{" "}
-      </h1>
-      <div className="flex flex-col  w-full lg:w-1/2">
-        <div className="flex gap-4 mx-auto w-full ">
-          <Input
-            error={formData["name"]?.error}
-            value={formData["name"]?.value}
-            name="name"
-            placeholder="Name"
-            className="w-full"
+    <>
+      <Message
+        message={notifMessage.message}
+        id={notifMessage.id}
+        type={notifMessage.type}
+      />
+      <form
+        ref={form}
+        action="/api/hello"
+        method="POST"
+        className="h-full container flex flex-col justify-center items-center  mx-auto gap-8"
+        target="lmao"
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendEmail();
+        }}
+      >
+        <h1 className="text-4xl">
+          Lets <span className="text-accent">Connect</span>{" "}
+        </h1>
+        <div className="flex flex-col  w-full lg:w-1/2">
+          <div className="flex gap-4 mx-auto w-full ">
+            <Input
+              error={formData["name"]?.error}
+              value={formData["name"]?.value}
+              name="name"
+              placeholder="Name"
+              className="w-full"
+              onChange={(e) => {
+                changeValue("name", e.target.value);
+              }}
+            />
+            <Input
+              error={formData["email"]?.error}
+              value={formData["email"]?.value}
+              name="email"
+              placeholder="Email"
+              type="email"
+              className="w-full"
+              onChange={(e) => {
+                changeValue("email", e.target.value);
+              }}
+            />
+          </div>
+          <div className="w-full">
+            <Input
+              error={formData["subject"]?.error}
+              value={formData["subject"]?.value}
+              name="subject"
+              placeholder="Subject"
+              className="w-full"
+              onChange={(e) => {
+                changeValue("subject", e.target.value);
+              }}
+            />
+          </div>
+          <textarea
+            name="message"
+            cols={30}
+            rows={10}
+            className="bg-transparent hover:outline-none focus:outline-none hover:border-accent/70 focus:border-accent border-2 border-gray-500/30 transition-colors ease-in-out duration-150 resize-none rounded-md p-2 mt-4"
+            value={formData["message"]?.value}
             onChange={(e) => {
-              changeValue("name", e.target.value);
+              changeValue("message", e.target.value);
             }}
-          />
-          <Input
-            error={formData["email"]?.error}
-            value={formData["email"]?.value}
-            name="email"
-            placeholder="Email"
-            type="email"
-            className="w-full"
-            onChange={(e) => {
-              changeValue("email", e.target.value);
-            }}
-          />
+          ></textarea>
+          <div className="w-full mt-4">
+            <button className="cursor-pointer border-2 border-gray-500/30 hover:border-accent hover:text-accent transition-colors ease-in duration-150 rounded-2xl w-fit py-2 px-4 text-sm mx-auto flex justify-center items-center gap-2">
+              Lets Talk
+              {isLoading && (
+                <span className="animate-spin  text-accent inline-block ">
+                  <BiLoader />
+                </span>
+              )}
+            </button>
+          </div>
         </div>
-        <div className="w-full">
-          <Input
-            error={formData["subject"]?.error}
-            value={formData["subject"]?.value}
-            name="subject"
-            placeholder="Subject"
-            className="w-full"
-            onChange={(e) => {
-              changeValue("subject", e.target.value);
-            }}
-          />
-        </div>
-        <textarea
-          name="message"
-          cols={30}
-          rows={10}
-          className="bg-transparent hover:outline-none focus:outline-none hover:border-accent/70 focus:border-accent border-2 border-gray-500/30 transition-colors ease-in-out duration-150 resize-none rounded-md p-2 mt-4"
-          value={formData["message"]?.value}
-          onChange={(e) => {
-            changeValue("message", e.target.value);
-          }}
-        ></textarea>
-        <div className="w-full mt-4">
-          <button className="cursor-pointer border-2 border-gray-500/30 hover:border-accent hover:text-accent transition-colors ease-in duration-150 rounded-2xl w-fit py-2 px-4 text-sm mx-auto flex justify-center items-center gap-2">
-            Lets Talk
-            {isLoading && (
-              <span className="animate-spin  text-accent inline-block ">
-                <BiLoader />
-              </span>
-            )}
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
