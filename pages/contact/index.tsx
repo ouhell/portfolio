357b51a20851a672a8f7615ae22fe24d4a776ae5
Oussama <oussamabarca200@gmail.com>
2024-03-06 13:04:29 +0100

    -fixed contact page overflow scroll on mobile view

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -148,74 +148,76 @@ const Contact = (props: Props) => {
         type={notifMessage.type}
       />
       <form
+        className="h-full overflow-y-auto     container mx-auto py-4"
         ref={form}
         action="/api/hello"
         method="POST"
-        className="h-full container flex flex-col justify-center items-center  mx-auto gap-8"
         target="lmao"
         onSubmit={(e) => {
           e.preventDefault();
           sendEmail();
         }}
       >
-        <h1 className="text-4xl">
-          Lets <span className="text-accent">Connect</span>{" "}
-        </h1>
-        <div className="flex flex-col  w-full lg:w-1/2">
-          <div className="flex gap-4 mx-auto w-full ">
-            <Input
-              error={formData["name"]?.error}
-              value={formData["name"]?.value}
-              name="name"
-              placeholder="Name"
-              className="w-full"
+        <div className="flex flex-col gap-8  justify-center items-center ">
+          <h1 className="text-4xl">
+            Lets <span className="text-accent">Connect</span>{" "}
+          </h1>
+          <div className="flex flex-col  w-full lg:w-1/2">
+            <div className="flex gap-4 mx-auto w-full ">
+              <Input
+                error={formData["name"]?.error}
+                value={formData["name"]?.value}
+                name="name"
+                placeholder="Name"
+                className="w-full"
+                onChange={(e) => {
+                  changeValue("name", e.target.value);
+                }}
+              />
+              <Input
+                error={formData["email"]?.error}
+                value={formData["email"]?.value}
+                name="email"
+                placeholder="Email"
+                type="email"
+                className="w-full"
+                onChange={(e) => {
+                  changeValue("email", e.target.value);
+                }}
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                error={formData["subject"]?.error}
+                value={formData["subject"]?.value}
+                name="subject"
+                placeholder="Subject"
+                className="w-full"
+                onChange={(e) => {
+                  changeValue("subject", e.target.value);
+                }}
+              />
+            </div>
+            <textarea
+              name="message"
+              cols={30}
+              rows={10}
+              className="bg-transparent hover:outline-none focus:outline-none hover:border-accent/70 focus:border-accent border-2 border-gray-500/30 transition-colors ease-in-out duration-150 resize-none rounded-md p-2 mt-4"
+              value={formData["message"]?.value}
               onChange={(e) => {
-                changeValue("name", e.target.value);
+                changeValue("message", e.target.value);
               }}
-            />
-            <Input
-              error={formData["email"]?.error}
-              value={formData["email"]?.value}
-              name="email"
-              placeholder="Email"
-              type="email"
-              className="w-full"
-              onChange={(e) => {
-                changeValue("email", e.target.value);
-              }}
-            />
-          </div>
-          <div className="w-full">
-            <Input
-              error={formData["subject"]?.error}
-              value={formData["subject"]?.value}
-              name="subject"
-              placeholder="Subject"
-              className="w-full"
-              onChange={(e) => {
-                changeValue("subject", e.target.value);
-              }}
-            />
-          </div>
-          <textarea
-            name="message"
-            cols={30}
-            rows={10}
-            className="bg-transparent hover:outline-none focus:outline-none hover:border-accent/70 focus:border-accent border-2 border-gray-500/30 transition-colors ease-in-out duration-150 resize-none rounded-md p-2 mt-4"
-            value={formData["message"]?.value}
-            onChange={(e) => {
-              changeValue("message", e.target.value);
-            }}
-          ></textarea>
-          <div className="w-full mt-4">
-            <button className="cursor-pointer border-2 border-gray-500/30 hover:border-accent hover:text-accent transition-colors ease-in duration-150 rounded-2xl w-fit py-2 px-4 text-sm mx-auto flex justify-center items-center gap-2">
-              Lets Talk
-              {isLoading && (
-                <span className="animate-spin  text-accent inline-block ">
-                  <BiLoader />
-                </span>
-              )}
-            </button>
+            ></textarea>
+            <div className="w-full mt-4">
+              <button className="cursor-pointer border-2 border-gray-500/30 hover:border-accent hover:text-accent transition-colors ease-in duration-150 rounded-2xl w-fit py-2 px-4 text-sm mx-auto flex justify-center items-center gap-2">
+                Lets Talk
+                {isLoading && (
+                  <span className="animate-spin  text-accent inline-block ">
+                    <BiLoader />
+                  </span>
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </form>
